095e73a91b33891c748229c131ff15cd0b58f408
SangayThinley <20095373@tafe.wa.edu.au>
2024-02-20 16:44:06 +0800

    feat(Step 4): create Player class

---
+++ app/player_node.py
@@ -0,0 +1,27 @@
+from player import Player
+class PlayerNode:
+    def __init__(self, player1):
+        self._player = player1
+        self._next = None
+        self._prev = None
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def next(self):
+        return self._next
+
+    @property
+    def prev(self):
+        return self._prev
+
+    @property
+    def key(self):
+        return self.player.uid
+
+    def __str__(self):
+        return f"{self.player.uid},{self.player.name}"
+
+
